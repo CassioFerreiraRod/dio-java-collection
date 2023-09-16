324872fd803c6e0659343a3891084c7fd35596ab
Cassio Ferreira <cassio1215@hotmail.com>
2023-09-16 18:40:22 -0300

    Adicionando Ordenacao

---
+++ src/list/Ordenacao/OrdenacaoNumeros.java
@@ -0,0 +1,59 @@
+package list.Ordenacao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoNumeros {
+    List<Integer> listaDeNumeros;
+
+    public OrdenacaoNumeros() {
+        listaDeNumeros = new ArrayList<>();
+    }
+
+    public void adicionarNumero(int numero) {
+        listaDeNumeros.add(numero);
+    }
+
+    public List<Integer> ordenarAscendente() {
+        List<Integer> numerosAscendente = new ArrayList<>(listaDeNumeros);
+        if (!listaDeNumeros.isEmpty()){
+            Collections.sort(numerosAscendente);
+            return numerosAscendente;
+        } else {
+            throw new RuntimeException("A lista esta vazia!");
+        }
+    }
+
+    public List<Integer> ordenarDescendente() {
+        List<Integer> numerosDescendente = new ArrayList<>(listaDeNumeros);
+        if (!listaDeNumeros.isEmpty()){
+            numerosDescendente.sort(Collections.reverseOrder());
+            return numerosDescendente;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+    public void exibirNumeros(){
+        System.out.println(listaDeNumeros);
+    }
+
+    public static void main(String[] args) {
+        OrdenacaoNumeros numeros = new OrdenacaoNumeros();
+
+        numeros.adicionarNumero(4);
+        numeros.adicionarNumero(6);
+        numeros.adicionarNumero(8);
+        numeros.adicionarNumero(2);
+        numeros.adicionarNumero(98);
+
+        numeros.exibirNumeros();
+
+        System.out.println(numeros.ordenarAscendente());
+
+        System.out.println(numeros.ordenarDescendente());
+
+        numeros.exibirNumeros();
+    }
+}
---
+++ src/list/Ordenacao/OrdenacaoPessoas.java
@@ -0,0 +1,57 @@
+package list.Ordenacao;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoPessoas {
+    List<Pessoa> listaDePessoas;
+
+    public OrdenacaoPessoas() {
+        this.listaDePessoas = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, int idade, double altura) {
+        listaDePessoas.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade() {
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(listaDePessoas);
+        if (!listaDePessoas.isEmpty()){
+            Collections.sort(pessoasPorIdade);
+            return pessoasPorIdade;
+        } else {
+            throw new RuntimeException("A lista esta vazia!");
+        }
+    }
+
+    public List<Pessoa> ordenarPorAltura() {
+        List<Pessoa> pessoasPorAltura = new ArrayList<>(listaDePessoas);
+        if (!listaDePessoas.isEmpty()) {
+            Collections.sort(pessoasPorAltura, new ComparatorPorAltura());
+            return pessoasPorAltura;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+    public void exivirPessoas() {
+        System.out.println(listaDePessoas);
+    }
+
+    public static void main(String[] args) {
+        // Criando uma instância da classe OrdenacaoPessoas
+        OrdenacaoPessoas ordenacaoPessoas = new OrdenacaoPessoas();
+
+        // Adicionando pessoas à lista
+        ordenacaoPessoas.adicionarPessoa("Alice", 20, 1.56);
+        ordenacaoPessoas.adicionarPessoa("Bob", 30, 1.80);
+        ordenacaoPessoas.adicionarPessoa("Charlie", 25, 1.70);
+        ordenacaoPessoas.adicionarPessoa("David", 17, 1.56);
+
+        ordenacaoPessoas.exivirPessoas();
+        System.out.println(ordenacaoPessoas.ordenarPorIdade());
+        System.out.println(ordenacaoPessoas.ordenarPorAltura());
+    }
+}
