9b3197eec8e3f21d7cb5d1dcafb0adde1c03abbf
Cassio Ferreira <cassio1215@hotmail.com>
2023-09-23 21:49:35 -0300

    Adicionando Ordenação com Map

---
+++ src/map/Ordenacao/AgendaEventos.java
@@ -0,0 +1,64 @@
+package map.Ordenacao;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.*;
+
+public class AgendaEventos {
+    private Map<LocalDate, Evento> eventoMap;
+
+    public AgendaEventos() {
+        this.eventoMap = new HashMap<>();
+    }
+
+    public void adicionarEvento(LocalDate data, String nome, String atracao) {
+//        Evento evento = new Evento(nome, atracao);
+        eventoMap.put(data, new Evento(nome, atracao));
+    }
+
+    public void exibirAgenda() {
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventoMap);
+        for (Map.Entry<LocalDate, Evento> entry: eventosTreeMap.entrySet()) {
+            LocalDate data = entry.getKey();
+            String eventoNome = String.valueOf(entry.getValue());
+
+            System.out.println("Data: " + data + eventoNome );
+        }
+    }
+
+    public void obterProximoEvento() {
+        LocalDate proximaData = null;
+        Evento proximoEvento = null;
+        LocalDate dataAtual = LocalDate.now();
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventoMap);
+        for (Map.Entry<LocalDate, Evento> entry : eventosTreeMap.entrySet()) {
+            if (entry.getKey().isEqual(dataAtual) || entry.getKey().isAfter(dataAtual)) {
+                proximaData = entry.getKey();
+                proximoEvento = entry.getValue();
+                System.out.println("O proximo evento: " + proximoEvento + " acontecerá na data " + proximaData);
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        AgendaEventos agendaEventos = new AgendaEventos();
+
+        // Adiciona eventos à agenda
+        agendaEventos.adicionarEvento(LocalDate.of(2022, Month.JULY, 15), "Conferência de Tecnologia", "Palestrante renomado");
+        agendaEventos.adicionarEvento(LocalDate.of(2022, 7, 9), "Workshop de Programação", "Aula prática de desenvolvimento");
+        agendaEventos.adicionarEvento(LocalDate.of(2000, 1, 10), "Lançamento de Software", "Demonstração da nova versão");
+        agendaEventos.adicionarEvento(LocalDate.of(2023, Month.SEPTEMBER, 23), "Hackathon de Inovação", "Competição de soluções criativas");
+        agendaEventos.adicionarEvento(LocalDate.of(2024, 9, 20), "Seminário de Inteligência Artificial", "Discussão sobre IA avançada");
+
+        // Exibe a agenda completa de eventos
+        agendaEventos.exibirAgenda();
+
+        // Obtém e exibe o próximo evento na agenda
+        agendaEventos.obterProximoEvento();
+
+        List<Character> consoantesMeuNome = Arrays.asList('c', 'm', 'l', 'c', 'v', 'l', 'c', 'n', 't');
+
+        System.out.println(consoantesMeuNome);
+    }
+}
